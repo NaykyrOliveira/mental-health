8d1a075afba51b49b304d45c817ebbe9463832ee
Naykyr Oliveira <naykyr@hotmail.com>
2024-08-17 17:31:36 -0300

    Adicionando icones no menu e ajustando algumas outras features

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,40 +1,45 @@
 <template>
     <div class="hero">
         <nav class="menu">
-        <div class="menu-toggle" @click="toggleMenu">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-        </div>
-        <div :class="['menu-items', { 'show-menu': isMenuOpen }]">
-            <router-link to="/" @click.native="closeMenu">Bem-vindo</router-link>
-            <router-link to="/result" @click.native="closeMenu">Meus Resultados</router-link>
-            <router-link to="/about" @click.native="closeMenu">Quem Somos</router-link>
-        </div>
+            <div class="menu-toggle" @click="toggleMenu">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+            <div :class="['menu-items', { 'show-menu': isMenuOpen }]">
+                <router-link to="/" @click.prevent="closeMenu">
+                    <font-awesome-icon :icon="['fas', 'home']" /> Bem-vindo
+                </router-link>
+                <router-link to="/result" @click.prevent="closeMenu">
+                    <font-awesome-icon :icon="['fas', 'chart-bar']" /> Meus Resultados
+                </router-link>
+                <router-link to="/about" @click.prevent="closeMenu">
+                    <font-awesome-icon :icon="['fas', 'info-circle']" /> Quem Somos
+                </router-link>
+            </div>
         </nav>
     </div>
 </template>
-  
 
 <script>
 export default {
     name: "Navbar",
     data() {
         return {
-        isMenuOpen: false
+            isMenuOpen: false
         };
     },
     methods: {
         toggleMenu() {
-        this.isMenuOpen = !this.isMenuOpen;
+            this.isMenuOpen = !this.isMenuOpen;
         },
         closeMenu() {
-        this.isMenuOpen = false;
+            this.isMenuOpen = false;
         }
     }
 }
 </script>
-  
+
 <style>
 @import '/public/assets/css/navbar.css';
 </style>
