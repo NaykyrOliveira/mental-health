6e448977ef375c6e1a5de867a744644580a5467a
Naykyr Oliveira <naykyr@hotmail.com>
2024-09-18 14:08:47 -0300

    Ajustes no menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,42 +1,24 @@
 <template>
-    <div class="hero">
-        <nav class="menu">
-            <div class="menu-toggle" @click="toggleMenu">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </div>
-            <div :class="['menu-items', { 'show-menu': isMenuOpen }]">
-                <router-link to="/" @click.prevent="closeMenu">
-                    <font-awesome-icon :icon="['fas', 'home']" /> Bem-vindo
-                </router-link>
-                <router-link to="/result" @click.prevent="closeMenu">
-                    <font-awesome-icon :icon="['fas', 'chart-bar']" /> Meus Resultados
-                </router-link>
-                <router-link to="/about" @click.prevent="closeMenu">
-                    <font-awesome-icon :icon="['fas', 'info-circle']" /> Quem Somos
-                </router-link>
-            </div>
-        </nav>
-    </div>
+  <header class="hero">
+    <nav class="menu">
+      <div class="menu-items">
+        <router-link to="/" @click="closeMenu">
+          <font-awesome-icon :icon="['fas', 'home']" />Bem-vindo
+        </router-link>
+        <router-link to="/result" @click="closeMenu">
+          <font-awesome-icon :icon="['fas', 'chart-bar']" />Meus Resultados
+        </router-link>
+        <router-link to="/about" @click="closeMenu">
+          <font-awesome-icon :icon="['fas', 'info-circle']" />Quem Somos
+        </router-link>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
-    name: "Navbar",
-    data() {
-        return {
-            isMenuOpen: false
-        };
-    },
-    methods: {
-        toggleMenu() {
-            this.isMenuOpen = !this.isMenuOpen;
-        },
-        closeMenu() {
-            this.isMenuOpen = false;
-        }
-    }
+  name: "Navbar",
 }
 </script>
 
