6a4a6896246e6b79a558165997272d1152f9088a
Naykyr Oliveira <naykyr@hotmail.com>
2024-12-30 13:53:59 -0300

    Remoção de pacotes

--- src/main.js
+++ src/main.js
@@ -2,14 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from '../store'; 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-library.add(fas);
+import 'bootstrap-icons/font/bootstrap-icons.css';  
 
 createApp(App)
   .use(router)
   .use(store)
-  .component('font-awesome-icon', FontAwesomeIcon)  
   .mount('#app');
